97c055f1183fe0a2a5abbc819035289934998966
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-28 16:46:44 +0000

    Allows a rook to move any number of spaces in one direction (not diagonally)

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,23 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+        let arr = [];
+
+        for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
+            if (j != col) {
+                arr.push(Square.at(row, j));
+            }            
+        }
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i != row) {
+                arr.push(Square.at(i, col));
+            }
+        }
+
+        return arr;
     }
 }
--- tests/engine/pieces/rook.spec.js
+++ tests/engine/pieces/rook.spec.js
@@ -1,5 +1,36 @@
 import Rook from '../../../src/engine/pieces/rook';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Rook', () => {
+
+    let board;
+    beforeEach(() => board = new Board()); 
+
+    it('can move any number of spaces in a straight line (no diagonals)', () => {
+        const rook = new Rook(Player.WHITE);
+        board.setPiece(Square.at(4, 1), rook);
+
+        const moves = rook.getAvailableMoves(board);
+            
+        moves.should.have.length(14);
+        moves.should.deep.include.members([            
+            Square.at(4, 0),
+            Square.at(4, 2),
+            Square.at(4, 3),
+            Square.at(4, 4),
+            Square.at(4, 5),
+            Square.at(4, 6),
+            Square.at(4, 7),    
+            Square.at(0, 1),
+            Square.at(1, 1),
+            Square.at(2, 1),
+            Square.at(3, 1),
+            Square.at(5, 1),
+            Square.at(6, 1),
+            Square.at(7, 1)       
+        ]);
+    });
 
 });
