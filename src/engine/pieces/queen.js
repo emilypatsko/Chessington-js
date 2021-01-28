c8f2bce4746e91c3579eccc423e7a193881b6a91
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-28 17:01:22 +0000

    Allows a queen to move any number of spaces in any one direction, including diagonally

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +8,61 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+        let arr = [];
+
+        // moves in a straight (not diagonal) line - same as rook logic
+        for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
+            if (j != col) {
+                arr.push(Square.at(row, j));
+            }            
+        }
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i != row) {
+                arr.push(Square.at(i, col));
+            }
+        }
+
+        // diagonal moves - same as bishop logic
+        // moves to the right and up
+        let i = row;
+        let j = col;
+        while (i < GameSettings.BOARD_SIZE - 1 && j < GameSettings.BOARD_SIZE - 1) {
+            arr.push(Square.at(i + 1, j + 1));
+            i++;
+            j++;
+        }
+
+        // moves to the right and down
+        i = row;
+        j = col;
+        while (i > 0 && j < GameSettings.BOARD_SIZE - 1) {
+            arr.push(Square.at(i - 1, j + 1));
+            i--;
+            j++;
+        }        
+
+        // moves to the left and down
+        i = row;
+        j = col;
+        while (i > 0 && j > 0) {
+            arr.push(Square.at(i - 1, j - 1));
+            i--;
+            j--;
+        }        
+
+        // moves to the left and up
+        i = row;
+        j = col;
+        while (i < GameSettings.BOARD_SIZE - 1 && j > 0) {
+            arr.push(Square.at(i + 1, j - 1));
+            i++;
+            j--;
+        }   
+
+        return arr;
     }
 }
--- tests/engine/pieces/queen.spec.js
+++ tests/engine/pieces/queen.spec.js
@@ -1,5 +1,47 @@
 import Queen from '../../../src/engine/pieces/queen';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Queen', () => {
+
+    let board;
+    beforeEach(() => board = new Board()); 
+
+    it('can move any number of spaces in any one direction, including diagonally', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(5, 4), queen);
+
+        const moves = queen.getAvailableMoves(board);
+            
+        moves.should.have.length(25);
+        moves.should.deep.include.members([  
+            Square.at(0, 4), 
+            Square.at(1, 4),
+            Square.at(2, 4),
+            Square.at(3, 4),
+            Square.at(4, 4),
+            Square.at(6, 4),
+            Square.at(7, 4),
+            Square.at(5, 0),
+            Square.at(5, 1),
+            Square.at(5, 2),
+            Square.at(5, 3),
+            Square.at(5, 5),
+            Square.at(5, 6),
+            Square.at(5, 7),
+            Square.at(6, 5), // top right diagonal
+            Square.at(7, 6),
+            Square.at(4, 5), // bottom right diagonal
+            Square.at(3, 6),
+            Square.at(2, 7),
+            Square.at(4, 3), // bottom left diagonal
+            Square.at(3, 2),
+            Square.at(2, 1),
+            Square.at(1, 0),
+            Square.at(6, 3), // top left diagonal
+            Square.at(7, 2)
+        ]);         
+    });
 
 });
