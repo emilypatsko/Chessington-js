fce119cf65c2aa8b7c56ea1b273c5dd0e0afbbf4
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-28 16:21:21 +0000

    Allows bishop to move any number of spaces, diagonally.

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,5 +1,4 @@
 import Piece from './piece';
-import Piece from './piece';
 import Square from '../square';
 import GameSettings from '../gameSettings';
 
@@ -9,6 +8,48 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+
+        let arr = [];
+
+        // moves to the right and up
+        let i = row;
+        let j = col;
+        while (i < GameSettings.BOARD_SIZE - 1 && j < GameSettings.BOARD_SIZE - 1) {
+            arr.push(Square.at(i + 1, j + 1));
+            i++;
+            j++;
+        }
+
+        // moves to the right and down
+        i = row;
+        j = col;
+        while (i > 0 && j < GameSettings.BOARD_SIZE - 1) {
+            arr.push(Square.at(i - 1, j + 1));
+            i--;
+            j++;
+        }        
+
+        // moves to the left and down
+        i = row;
+        j = col;
+        while (i > 0 && j > 0) {
+            arr.push(Square.at(i - 1, j - 1));
+            i--;
+            j--;
+        }        
+
+        // moves to the left and up
+        i = row;
+        j = col;
+        while (i < GameSettings.BOARD_SIZE - 1 && j > 0) {
+            arr.push(Square.at(i + 1, j - 1));
+            i++;
+            j--;
+        }   
+
+        return arr;
     }
 }
--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -5,4 +5,30 @@ import Square from '../../../src/engine/square';
 
 describe('Bishop', () => {
 
+    let board;
+    beforeEach(() => board = new Board()); 
+
+    it('can move any number of spaces along a diagonal', () => {
+        const bishop = new Bishop(Player.WHITE); // does it matter to test this for black as well?
+        board.setPiece(Square.at(3, 3), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+            
+        moves.should.have.length(13);
+        moves.should.deep.include.members([
+            Square.at(0, 0),
+            Square.at(1, 1),
+            Square.at(2, 2),
+            Square.at(4, 4),
+            Square.at(5, 5),
+            Square.at(6, 6),
+            Square.at(7, 7),
+            Square.at(6, 0),
+            Square.at(5, 1),
+            Square.at(4, 2),
+            Square.at(2, 4),
+            Square.at(1, 5),
+            Square.at(0, 6)
+        ]);
+    });
 });
