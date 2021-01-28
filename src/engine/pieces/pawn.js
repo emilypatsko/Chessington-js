26be7426c348a7dec13275a2635c8d30e1b746e4
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-28 12:24:42 +0000

    Allows pawns to move one space up or down depending on if the player is white or black.

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,3 +1,6 @@
+import GameSettings from '../gameSettings';
+import Player from '../player';
+import Square from '../square';
 import Piece from './piece';
 
 export default class Pawn extends Piece {
@@ -6,6 +9,15 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        // This method will return all moves that can be made by a pawn
+        
+        // Find where our piece is. If the player is white, the pawn can move up a space
+        // If the player is black, the pawn can move down a space
+        let location = board.findPiece(this);
+        if (this.player === Player.WHITE) {
+            return Square.at(location.row + 1, location.col)
+        } else {
+            return Square.at(location.row - 1, location.col)
+        }
     }
 }
