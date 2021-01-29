a612651208c608da94f40ab43caccf34e6c32de4
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-29 12:17:08 +0000

    Added edge cases for the knight

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -11,7 +11,8 @@ export default class Knight extends Piece {
         let row = location.row;
         let col = location.col;
 
-        return [
+        // Moves that a knight can ordinarily make
+        let moves = [ 
             Square.at(row - 2, col - 1),
             Square.at(row - 2, col + 1),
             Square.at(row - 1, col - 2),
@@ -20,6 +21,17 @@ export default class Knight extends Piece {
             Square.at(row + 1, col + 2),
             Square.at(row + 2, col - 1),
             Square.at(row + 2, col + 1)
-        ]
+        ];
+
+        // Only return the moves that don't go off the board
+        let arr = [];
+
+        moves.forEach(move => {
+            if (move.row >= 0 && move.row <= 7 && move.col >= 0 && move.col <= 7) {
+                arr.push(move);
+            }
+        });
+
+        return arr;
     }
 }
--- tests/engine/pieces/knight.spec.js
+++ tests/engine/pieces/knight.spec.js
@@ -9,14 +9,8 @@ describe('Knight', () => {
     let board;
     beforeEach(() => board = new Board()); 
 
-    it('can move in an L shape in any direction', () => {
-        const knight = new Knight(Player.WHITE);
-        board.setPiece(Square.at(2, 2), knight);
-
-        const moves = knight.getAvailableMoves(board);
-            
-        moves.should.have.length(8);
-        moves.should.deep.include.members([
+    let testCases = [
+        { caseType: "normal", squareToCheck: Square.at(2, 2), movesLength: 8, movesAvailable: [
             Square.at(0, 1),
             Square.at(0, 3),
             Square.at(1, 0),
@@ -25,6 +19,30 @@ describe('Knight', () => {
             Square.at(3, 4),
             Square.at(4, 1),
             Square.at(4, 3)
-        ]);
-    });
+        ]},
+        { caseType: "edge case", squareToCheck: Square.at(1, 6), movesLength: 4, movesAvailable: [
+            Square.at(0, 4),
+            Square.at(2, 4),
+            Square.at(3, 5),
+            Square.at(3, 7)
+        ]},
+        { caseType: "corner case", squareToCheck: Square.at(7, 0), movesLength: 2, movesAvailable: [
+            Square.at(5, 1),
+            Square.at(6, 2)
+        ]}
+    ];   
+    
+    testCases.forEach(testCase => {
+        it(`can move in an L shape in any direction (${testCase.caseType})`, () => {
+            const knight = new Knight(Player.WHITE);
+            board.setPiece(testCase.squareToCheck, knight);
+    
+            const moves = knight.getAvailableMoves(board);
+                
+            moves.should.have.length(testCase.movesLength);
+            if (testCase.movesLength != 0) {
+                moves.should.deep.include.members(testCase.movesAvailable);
+            };            
+        });
+    });    
 });
